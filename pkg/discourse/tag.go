c890dcceff9c0bf8202c69d62ecee81f9c08463e
Lena Voytek <lena@voytek.dev>
2024-06-03 13:59:10 -0700

    Add some tag structs

---
+++ pkg/discourse/tag.go
@@ -0,0 +1,25 @@
+package discourse
+
+type Tag struct {
+	ID        int    `json:"id"`
+	Text      string `json:"text"`
+	Count     int    `json:"count"`
+	PMCount   int    `json:"pm_count"`
+	TargetTag string `json:"target_tag"`
+}
+
+type TagGroup struct {
+	ID            int         `json:"id"`
+	Name          string      `json:"name"`
+	TagNames      []string    `json:"tag_names"`
+	ParentTagName []string    `json:"parent_tag_name"`
+	OnePerTopic   bool        `json:"one_per_topic"`
+	Permissions   Permissions `json:"permissions"`
+}
+
+type ListTagsResponse struct {
+	Tags   []Tag `json:"tags"`
+	Extras struct {
+		Categories []Category `json:"categories,omitempty"`
+	} `json:"extras,omitempty"`
+}
