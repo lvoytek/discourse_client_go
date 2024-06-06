9cebf7e253bc71869e973cffdd3a6c4df5ae631b
Lena Voytek <lena@voytek.dev>
2024-06-06 15:24:18 -0700

    Add post structs

--- pkg/discourse/post.go
+++ pkg/discourse/post.go
@@ -1,1 +1,77 @@
 package discourse
+
+type NewPost struct {
+	Title             string `json:"title"`
+	Raw               string `json:"raw"`
+	TopicID           int    `json:"topic_id"`
+	Category          int    `json:"category"`
+	TargetRecipients  string `json:"target_recipients"`
+	TargetUsernames   string `json:"target_usernames"`
+	Archetype         string `json:"archetype"`
+	CreatedAt         string `json:"created_at"`
+	ReplyToPostNumber int    `json:"reply_to_post_number"`
+	EmbedURL          string `json:"embed_url"`
+	ExternalID        string `json:"external_id"`
+}
+
+type PostData struct {
+	ID                          int          `json:"id"`
+	Username                    string       `json:"username"`
+	AvatarTemplate              string       `json:"avatar_template"`
+	CreatedAt                   string       `json:"created_at"`
+	Cooked                      string       `json:"cooked"`
+	PostNumber                  int          `json:"post_number"`
+	PostType                    int          `json:"post_type"`
+	UpdatedAt                   string       `json:"updated_at"`
+	ReplyCount                  int          `json:"reply_count"`
+	ReplyToPostNumber           string       `json:"reply_to_post_number"`
+	QuoteCount                  int          `json:"quote_count"`
+	IncomingLinkCount           int          `json:"incoming_link_count"`
+	Reads                       int          `json:"reads"`
+	ReadersCount                int          `json:"readers_count"`
+	Score                       int          `json:"score"`
+	Yours                       bool         `json:"yours"`
+	TopicID                     int          `json:"topic_id"`
+	TopicSlug                   string       `json:"topic_slug"`
+	PrimaryGroupName            string       `json:"primary_group_name"`
+	FlairName                   string       `json:"flair_name"`
+	FlairURL                    string       `json:"flair_url"`
+	FlairBgColor                string       `json:"flair_bg_color"`
+	FlairColor                  string       `json:"flair_color"`
+	FlairGroupID                string       `json:"flair_group_id"`
+	Version                     int          `json:"version"`
+	CanEdit                     bool         `json:"can_edit"`
+	CanDelete                   bool         `json:"can_delete"`
+	CanRecover                  bool         `json:"can_recover"`
+	CanSeeHiddenPost            bool         `json:"can_see_hidden_post"`
+	CanWiki                     bool         `json:"can_wiki"`
+	UserTitle                   string       `json:"user_title"`
+	Bookmarked                  bool         `json:"bookmarked"`
+	Raw                         string       `json:"raw"`
+	ActionsSummary              []PostAction `json:"actions_summary"`
+	Moderator                   bool         `json:"moderator"`
+	Admin                       bool         `json:"admin"`
+	Staff                       bool         `json:"staff"`
+	UserID                      int          `json:"user_id"`
+	Hidden                      bool         `json:"hidden"`
+	TrustLevel                  int          `json:"trust_level"`
+	DeletedAt                   string       `json:"deleted_at"`
+	UserDeleted                 bool         `json:"user_deleted"`
+	EditReason                  string       `json:"edit_reason"`
+	CanViewEditHistory          bool         `json:"can_view_edit_history"`
+	Wiki                        bool         `json:"wiki"`
+	ReviewableID                string       `json:"reviewable_id"`
+	ReviewableScoreCount        int          `json:"reviewable_score_count"`
+	ReviewableScorePendingCount int          `json:"reviewable_score_pending_count"`
+	MentionedUsers              []string     `json:"mentioned_users"`
+	Name                        string       `json:"name"`
+	DisplayUsername             string       `json:"display_username"`
+}
+
+type PostAction struct {
+	ID      int  `json:"id"`
+	Count   int  `json:"count"`
+	Acted   bool `json:"acted"`
+	CanUndo bool `json:"can_undo"`
+	CanAct  bool `json:"can_act"`
+}
